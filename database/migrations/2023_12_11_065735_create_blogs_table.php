a4f487e156a49b1bb246320738139b079c2962ff
farad-tech <f1378321.karami@gmail.com>
2023-12-11 10:34:08 +0330

    blog model and migration created completely

---
+++ database/migrations/2023_12_11_065735_create_blogs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('image');
+            $table->text('content');
+            $table->text('category');
+            $table->text('keywords');
+            $table->text('author')->default(Auth::id());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
