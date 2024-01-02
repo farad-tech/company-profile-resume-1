6b760321a7fad83c7ff7050cbcdfba3b5c7d0000
farad-tech <f1378321.karami@gmail.com>
2024-01-02 14:16:12 +0330

    header styles and footer scripts changed

--- resources/views/public/layouts/joint/style.blade.php
+++ resources/views/public/layouts/joint/style.blade.php
@@ -1,12 +1,3 @@
-<!-- Google Web Fonts -->
-{{-- <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap"
-    rel="stylesheet"> --}}
-
-<!-- Icon Font Stylesheet -->
-{{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"> --}}
-
 <!-- Libraries Stylesheet -->
 <link href="{{asset('public/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
 <link href="{{asset('public/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')}}" rel="stylesheet" />
